515d378e2d3852ba3516cefe0b5ec35ccb6ab42d
EZENGESH <ezengesh@gmail.com>
2025-09-03 10:12:02 +0300

    fix: wrap children in AdminLayout for PrivateRoute component

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -9,5 +9,9 @@ export default function PrivateRoute({ children }) {
     return <Navigate to="/login" replace />;
   }
 
-  return children;
+   return (
+        <AdminLayout>
+            {children}
+        </AdminLayout>
+    );
 }
