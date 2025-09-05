14aec0f75b36871bb8dc66ddf1dc7e11b0552571
EZENGESH <ezengesh@gmail.com>
2025-09-06 00:58:01 +0300

    fix: correct import path for UserServiceAuthentication and ensure rest_framework.authtoken is included in installed apps

--- requisition-service/requisition_service/settings.py
+++ requisition-service/requisition_service/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'requisition_service',
     'corsheaders',
-
+    'rest_framework.authtoken',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -53,7 +53,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'requisitions.authentication.UserServiceAuthentication',
+        'requisition_service.authentication.UserServiceAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
