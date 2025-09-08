6b40dbccf8813908cb144c31d40d804ff0d54044
Ngeno Kiprotich Ezekiel <ezengesh@gmail.com>
2025-09-08 22:55:54 +0300

    updated

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -106,13 +106,7 @@ const createService = (baseURL, serviceName) => {
     timeout: 10000,
   });
 
-  requisitionApi.interceptors.request.use((config) => {
-    const token = localStorage.getItem("access_token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  });
+  
 
   attachToken(instance);
   attachRefresh(instance);
@@ -128,6 +122,14 @@ export const api = createService(SERVICES.user, "User");
 export const inventoryApi = createService(SERVICES.inventory, "Inventory");
 export const requisitionApi = createService(SERVICES.requisition, "Requisition");
 export const firearmApi = createService(SERVICES.firearmLog, "Firearm Log");
+
+requisitionApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default {
   api,
