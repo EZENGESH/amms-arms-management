f403eedb12814535c110d247b5a7301f53a55190
Ngeno Kiprotich Ezekiel <ezengesh@gmail.com>
2025-09-09 20:27:15 +0300

    add os path

--- inventory-service/inventory_service/settings.py
+++ inventory-service/inventory_service/settings.py
@@ -10,7 +10,7 @@ from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Security settings
 SECRET_KEY = config('DJANGO_SECRET_KEY')
@@ -102,12 +102,6 @@ import os
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
- 
-
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
