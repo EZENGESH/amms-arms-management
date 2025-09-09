55af352405d651c25d66f162ba27499153bf3d84
Ngeno Kiprotich Ezekiel <ezengesh@gmail.com>
2025-09-09 20:36:47 +0300

    added

--- inventory-service/inventory_service/settings.py
+++ inventory-service/inventory_service/settings.py
@@ -10,7 +10,7 @@ from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security settings
 SECRET_KEY = config('DJANGO_SECRET_KEY')
