c46956cc6e21b3fbb4b9300b7903f4dae59673f2
Ngeno Kiprotich Ezekiel <ezengesh@gmail.com>
2025-09-09 18:37:56 +0300

    up

--- inventory-service/inventory_service/urls.py
+++ inventory-service/inventory_service/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ArmViewSet
 from django.http import JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def health_check(request):
     return JsonResponse({"status": "ok"})
@@ -14,4 +16,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/health/', health_check),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
