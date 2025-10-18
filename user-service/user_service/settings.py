ba6d2ce06e767babeb42fb6eafc8a1b1b236e2e8
EZENGESH <ezengesh@gmail.com>
2025-10-18 12:26:14 +0300

    updated

--- user-service/user_service/settings.py
+++ user-service/user_service/settings.py
@@ -203,6 +203,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8001",
     "http://127.0.0.1:8001",
+    "http://localhost:8003",
+    "http://127.0.0.1:8003",
+    "http://localhost:8004",
+    "http://127.0.0.1:8004"
 ]
 CORS_ALLOW_HEADERS = [
     "accept",
